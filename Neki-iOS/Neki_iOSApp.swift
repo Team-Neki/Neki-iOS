d5f2d6e76a22a81938a3bcead79b61288e70e5cb
OneTen19 <khy000426@naver.com>
2025-12-21 18:03:34 +0900

    Initial Commit

---
+++ Neki-iOS/Neki_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neki_iOSApp.swift
+//  Neki-iOS
+//
+//  Created by OneTen on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Neki_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

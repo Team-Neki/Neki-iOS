7632162cb9080512df106e042d76e5df29c5dcf5
SwainYun <destap@naver.com>
2025-12-22 21:02:36 +0900

    [Feat] #4 - Neki FontStyle 선언형 수정자 및 슈가코드 추가

--- Neki-iOS/Shared/DesignSystem/Sources/Font/Font+Pretendard.swift
+++ Neki-iOS/Shared/DesignSystem/Sources/Font/Font+Pretendard.swift
@@ -1,3 +1,10 @@
+//
+//  Font+Pretendard.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 12/22/25.
+//
+
 import SwiftUI
 
 public enum FontStyle: CaseIterable {
@@ -47,5 +54,42 @@ public enum FontStyle: CaseIterable {
         case .baseMedium, .largeMedium, .smallMedium, .extraLargeMedium, .extraSmallMedium: "Pretendard-Medium"
         case .baseRegular, .largeRegular, .smallRegular, .extraSmallRegular: "Pretendard-Regular"
         }
+    }
+}
+
+struct FontStyleModifier: ViewModifier {
+    public let style: FontStyle
+    
+    public init(style: FontStyle) {
+        self.style = style
+    }
+    
+    private var calculatedLineSpacing: CGFloat { style.lineHeight - style.fontSize }
+    private var calculatedTracking: CGFloat { style.letterSpacing }
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.custom(style.fontName, size: style.fontSize))
+            .fontWeight(style.fontWeight)
+            .lineSpacing(calculatedLineSpacing)
+            .tracking(calculatedTracking)
+    }
+}
+
+public extension View where Self == Text {
+    /// Neki 폰트 스타일을 적용합니다.
+    func nekiFont(_ style: FontStyle) -> some View {
+        modifier(FontStyleModifier(style: style))
+    }
+}
+
+public extension Font {
+    /// Neki 폰트를 사용합니다.
+    ///
+    /// - Attention: Neki 스타일을 제외한 폰트만을 사용하려고 할때 유용합니다. 일부 스타일은 누락될 수 있습니다.
+    ///     * 줄간격
+    ///     * 자간
+    static func neki(_ style: FontStyle) -> Self {
+        .custom(style.fontName, size: style.fontSize).weight(style.fontWeight)
     }
 }

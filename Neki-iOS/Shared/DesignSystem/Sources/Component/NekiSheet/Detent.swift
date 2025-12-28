bafbfcd8a9e5e70e3b6ecbb41be95c96d4ff5ee0
SwainYun <destap@naver.com>
2025-12-28 19:01:24 +0900

    [Feat] #7 - 시트 높이 Detent 정의

---
+++ Neki-iOS/Shared/DesignSystem/Sources/Component/NekiSheet/Detent.swift
@@ -0,0 +1,28 @@
+//
+//  Detent.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 12/28/25.
+//
+
+import Foundation
+
+public enum Detent: Hashable {
+    /// 화면 높이의 비율
+    case fraction(CGFloat)
+    /// 절대 높이
+    case absolute(CGFloat)
+    /// 화면 높이의 절반
+    case medium
+    /// 화면 최대 높이
+    case large
+    
+    func resolve(in height: CGFloat) -> CGFloat {
+        switch self {
+        case .fraction(let value): return height * value
+        case .absolute(let value): return value
+        case .medium: return height * 0.5
+        case .large: return height
+        }
+    }
+}

38f7be474a5885f6657c7ed8849736bbf39b63f9
SwainYun <destap@naver.com>
2025-12-29 22:21:50 +0900

    [Feat] #9 - 브랜드 종류, 포토부스 정보 표현 타입 선언

---
+++ Neki-iOS/Features/Map/Sources/Domain/Sources/Entities/PhotoBoothBrand.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoBoothBrand.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 12/29/25.
+//
+
+import Foundation
+
+/// 포토부스 브랜드의 종류입니다.
+public enum PhotoBoothBrand: CaseIterable {
+    case life4cut
+    case photoism
+    case photogray
+    case photosignature
+    case planBStudio
+    case monomansion
+    case theFilm
+    case insphoto
+    
+    public var displayName: String {
+        switch self {
+        case .life4cut: "인생네컷"
+        case .photoism: "포토이즘"
+        case .photogray: "포토 그레이"
+        case .photosignature: "포토 시그니처"
+        case .planBStudio: "플랜 비 스튜디오"
+        case .monomansion: "모노맨션"
+        case .theFilm: "더필름"
+        case .insphoto: "인스포토"
+        }
+    }
+}

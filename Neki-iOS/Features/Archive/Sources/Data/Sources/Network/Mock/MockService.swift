179db50594ddc3c517b3ff3153d3f650eb746ed2
OneTen19 <khy000426@naver.com>
2025-12-30 03:04:53 +0900

    [Feat] #6 - 사용예시를 위한 MockService

--- Neki-iOS/Features/Archive/Sources/Data/Sources/Network/Mock/MockService.swift
+++ Neki-iOS/Features/Archive/Sources/Data/Sources/Network/Mock/MockService.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol MockServiceProtocol {
+    func fetchMockData(page: Int, category: String) async throws -> MockResponseDTO
+}
+
+final class MockService: MockServiceProtocol {
+    
+    private let provider = NetworkProvider.shared
+    
+    func fetchMockData(page: Int, category: String) async throws -> MockResponseDTO {
+        return try await provider.request(endpoint: MockAPI.getTest(page: page, category: category))
+    }
+}

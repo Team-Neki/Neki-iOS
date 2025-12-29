e582c0df5a99ea1c6253b999e27d81f3d958559c
OneTen19 <khy000426@naver.com>
2025-12-30 03:05:04 +0900

    [Feat] #6 - 사용예시를 위한 DefaultMockRepository

--- Neki-iOS/Features/Archive/Sources/Data/Sources/Repositories/DefaultMockRepository.swift
+++ Neki-iOS/Features/Archive/Sources/Data/Sources/Repositories/DefaultMockRepository.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+// import ArchiveDomain
+
+public final class DefaultMockRepository: MockRepository {
+    
+    private let service: MockServiceProtocol
+    
+    init(service: MockServiceProtocol) {
+        self.service = service
+    }
+    
+    public func fetchMockList(page: Int, category: String) async throws -> MockEntity {
+        let result = try await service.fetchMockData(page: page, category: category)
+        
+        return result.toEntity()
+    }
+}

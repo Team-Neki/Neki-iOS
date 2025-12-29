8a3df6d0ee25fafd2323e0e0cbf2160e6342aaf9
OneTen19 <khy000426@naver.com>
2025-12-29 21:39:24 +0900

    [Feat] #6 - NetworkError

---
+++ Neki-iOS/Core/Sources/Network/Sources/Foundation/NetworkError.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkError.swift
+//  Neki-iOS
+//
+//  Created by OneTen on 12/29/25.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case apiError(message: String)
+    case requestEncodingError
+    case responseDecodingError
+    case responseError
+    case notFound
+    case internalServerError
+    case networkFail
+    case unknownError
+    
+    var errorDescription: String? {
+        switch self {
+        case .apiError(let message): return message
+        case .requestEncodingError: return "요청 인코딩에 실패했습니다."
+        case .responseDecodingError: return "응답 디코딩에 실패했습니다."
+        case .responseError: return "응답 오류가 발생했습니다."
+        case .notFound: return "리소스를 찾을 수 없습니다"
+        case .internalServerError: return "서버 내부 오류가 발생했습니다"
+        case .networkFail: return "네트워크 연결에 실패했습니다"
+        case .unknownError: return "알 수 없는 오류가 발생했습니다."
+        }
+    }
+}

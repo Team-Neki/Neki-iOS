2f7478f6f3f97ad85474afa961ecf9405098f9dd
OneTen19 <khy000426@naver.com>
2025-12-31 21:46:23 +0900

    [Chore] #6 - URLComponent 조립 및 URLRequest 생성 책임 소유자 변경 NetworkProvider -> Endpoint

--- Neki-iOS/Core/Sources/Network/Sources/Base/Endpoint.swift
+++ Neki-iOS/Core/Sources/Network/Sources/Base/Endpoint.swift
@@ -19,4 +19,65 @@ public protocol Endpoint {
     var method: HTTPMethodType { get }
     var body: Encodable? { get }
     var query: [URLQueryItem]? { get }
+    
+    func asURLRequest() throws -> URLRequest
+}
+
+extension Endpoint {
+    var baseURL: String {
+        guard let urlString = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            fatalError("🚨Base URL을 찾을 수 없습니다🚨")
+        }
+        return urlString
+    }
+    
+    public func asURLRequest() throws -> URLRequest {
+        guard let url = URL(string: baseURL)?.appending(path: path),
+              var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            throw NetworkError.invalidURLError
+        }
+        
+        if let query = query {
+            components.queryItems = query
+        }
+        
+        guard let finalURL = components.url else {
+            throw NetworkError.invalidURLError
+        }
+        
+        var request = URLRequest(url: finalURL)
+        request.httpMethod = method.rawValue
+        
+        let allHeaders = makeHeaders()
+        request.allHTTPHeaderFields = allHeaders
+        
+        if let body = body {
+            do {
+                request.httpBody = try JSONEncoder().encode(body)
+            } catch {
+                throw NetworkError.requestEncodingError
+            }
+        }
+        
+        return request
+    }
+    
+    func makeHeaders() -> [String: String] {
+        var headers: [String: String] = [
+            "Content-Type": "application/json"
+        ]
+        
+        switch headerType {
+        case .noneHeader:
+            break
+        case .accessTokenHeader:
+            // TODO: - 여기에 토큰 가져오는 로직 추가 (토큰매니저나 키체인매니저 등)
+            break
+        case .refreshTokenHeader:
+            // TODO: - 리프레시 토큰 로직 추가
+            break
+        }
+        
+        return headers
+    }
 }
--- Neki-iOS/Core/Sources/Network/Sources/DefaultNetworkProvider.swift
+++ Neki-iOS/Core/Sources/Network/Sources/DefaultNetworkProvider.swift
@@ -21,32 +21,18 @@ public final class DefaultNetworkProvider: NetworkProvider {
     /// voidResponse를 수행합니다.
     /// HTTP 200~299 상태 코드는 Void 값을 반환합니다.
     public func request(endpoint: Endpoint) async throws -> Void {
-        guard let url = makeURL(endpoint: endpoint) else {
-            throw NetworkError.invalidURLError
-        }
-
-        let request = try makeURLRequest(url: url, endpoint: endpoint)
-        
+        let request = try endpoint.asURLRequest()
         
         // 네트워크 검증을 위한 request 로그 출력
-        Logger.network.debug("➡️ [REQUEST] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "")")
-        if let headers = request.allHTTPHeaderFields {
-            Logger.network.debug("🧾 Headers: \(headers.description)")
-        }
-        if let body = request.httpBody,
-           let bodyString = String(data: body, encoding: .utf8) {
-            Logger.network.debug("📦 Body: \(bodyString)")
-        }
-        
+        logRequest(request)
         
         // 네트워크 요청
         do {
             let (data, response) = try await session.data(for: request, delegate: nil)
             try voidResponse(data: data, response: response)
-            return ()
         } catch {
             Logger.network.error("❌ Network Error: \(error.localizedDescription)")
-            throw NetworkError.networkFail
+            throw error
         }
     }
     
@@ -55,14 +41,25 @@ public final class DefaultNetworkProvider: NetworkProvider {
     /// decodableResponse를 수행합니다.
     /// HTTP 200~299 상태 코드는 `JSONDecoder`를 통해 `T` 타입으로 디코딩하여 반환합니다.
     public func request<T: Decodable>(endpoint: Endpoint) async throws -> T {
-        guard let url = makeURL(endpoint: endpoint) else {
-            throw NetworkError.invalidURLError
-        }
-
-        let request = try makeURLRequest(url: url, endpoint: endpoint)
-        
+        let request = try endpoint.asURLRequest()
         
         // 네트워크 검증을 위한 request 로그 출력
+        logRequest(request)
+        
+        // 네트워크 요청
+        do {
+            let (data, response) = try await session.data(for: request, delegate: nil)
+            return try decodableResponse(data: data, response: response)
+        } catch {
+            Logger.network.error("❌ Network Error: \(error.localizedDescription)")
+            throw error
+        }
+    }
+    
+}
+
+private extension DefaultNetworkProvider {
+    func logRequest(_ request: URLRequest) {
         Logger.network.debug("➡️ [REQUEST] \(request.httpMethod ?? "") \(request.url?.absoluteString ?? "")")
         if let headers = request.allHTTPHeaderFields {
             Logger.network.debug("🧾 Headers: \(headers.description)")
@@ -71,79 +68,6 @@ public final class DefaultNetworkProvider: NetworkProvider {
            let bodyString = String(data: body, encoding: .utf8) {
             Logger.network.debug("📦 Body: \(bodyString)")
         }
-        
-        
-        // 네트워크 요청
-        do {
-            let (data, response) = try await session.data(for: request, delegate: nil)
-            return try decodableResponse(data: data, response: response)
-        } catch {
-            Logger.network.error("❌ Network Error: \(error.localizedDescription)")
-            throw NetworkError.networkFail
-        }
-    }
-
-}
-
-private extension DefaultNetworkProvider {
-    func makeURL(endpoint: Endpoint) -> URL? {
-        // BASE URL 불러오기
-        guard let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
-            fatalError("🚨Base URL을 찾을 수 없습니다🚨")
-        }
-        
-        // URLComponents를 이용한 초기 URL 생성
-        guard let url = URL(string: baseURL)?.appending(path: endpoint.path),
-              var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else { return nil }
-                
-        // 쿼리 아이템이 있으면 추가
-        if let queryItems = endpoint.query {
-            components.queryItems = queryItems
-        }
-        
-        // 쿼리아이템이 추가된 최종 URL 반환
-        return components.url
-    }
-    
-    func makeHeaders(endpoint: Endpoint) -> [String: String] {
-        var headers: [String: String] = [
-            "Content-Type": "application/json"
-        ]
-        
-        switch endpoint.headerType {
-        case .noneHeader:
-            break
-        case .accessTokenHeader:
-            // TODO: - 여기에 토큰 가져오는 로직 추가 (토큰매니저나 키체인매니저 등)
-            break
-        case .refreshTokenHeader:
-            // TODO: - 리프레시 토큰 로직 추가
-            break
-        }
-        
-        return headers
-    }
-    
-    func makeURLRequest(url: URL, endpoint: Endpoint) throws -> URLRequest {
-        // Request 생성
-        var request = URLRequest(url: url)
-        request.httpMethod = endpoint.method.rawValue
-        
-        // 헤더 설정
-        let allHeaders = makeHeaders(endpoint: endpoint)
-        request.allHTTPHeaderFields = allHeaders
-        
-        // Body 인코딩
-        if let body = endpoint.body {
-            do {
-                request.httpBody = try JSONEncoder().encode(body)
-            } catch {
-                throw NetworkError.requestEncodingError
-            }
-        }
-        
-        // 최종 request 반환
-        return request
     }
     
     func voidResponse(data: Data, response: URLResponse) throws {

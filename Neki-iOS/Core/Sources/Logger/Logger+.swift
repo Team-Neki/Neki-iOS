3ab6a6cfc5605c56105d093fb03536b53e605c1c
OneTen19 <khy000426@naver.com>
2025-12-31 22:11:32 +0900

    [Chore] #6 - static logger들의 접근 제어자를 public으로 변경

--- Neki-iOS/Core/Sources/Logger/Logger+.swift
+++ Neki-iOS/Core/Sources/Logger/Logger+.swift
@@ -14,14 +14,14 @@ extension Logger {
     // MARK: - 로그를 성격별로 나누어 사용합니다
     
     /// 네트워크 통신 관련
-    static let network = Logger(subsystem: subsystem, category: "Network")
+    public static let network = Logger(subsystem: subsystem, category: "Network")
     
     /// 데이터 관련
-    static let data = Logger(subsystem: subsystem, category: "Data")
+    public static let data = Logger(subsystem: subsystem, category: "Data")
     
     /// Presentation 관련
-    static let presentation = Logger(subsystem: subsystem, category: "Presentation")
+    public static let presentation = Logger(subsystem: subsystem, category: "Presentation")
     
     /// 도메인 관련
-    static let domain = Logger(subsystem: subsystem, category: "Domain")
+    public static let domain = Logger(subsystem: subsystem, category: "Domain")
 }

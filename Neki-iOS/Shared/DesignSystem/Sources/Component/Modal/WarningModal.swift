874f0b3b3a4fd0528d54b77cde76f697a6da2edd
OneTen19 <khy000426@naver.com>
2026-01-04 17:40:30 +0900

    [Feat] #19 - WarningModal

---
+++ Neki-iOS/Shared/DesignSystem/Sources/Component/Modal/WarningModal.swift
@@ -0,0 +1,54 @@
+//
+//  WarningModal.swift
+//  Neki-iOS
+//
+//  Created by OneTen on 1/4/26.
+//
+
+import SwiftUI
+
+struct WarningModal: View {
+    
+    // MARK: - Properties
+    
+    let titleMessage: String
+    let onExit: () -> Void
+    
+    //MARK: - init
+
+    init(
+        titleMessage: String,
+        onExit: @escaping () -> Void
+    ) {
+        self.titleMessage = titleMessage
+        self.onExit = onExit
+    }
+    
+    // MARK: - Main Body
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Image(.iconCircleAlertFill)
+                .padding(.top, 20)
+            
+            Text(titleMessage)
+                .nekiFont(.body14Regular)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.gray500)
+                .padding(.top, 12)
+                .padding(.bottom, 20)
+        }
+        .frame(maxWidth: .infinity)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .overlay(
+            Button(action: {
+                onExit()
+            }) {
+                Image(.iconXmarkBlack)
+                    .padding(12)
+            }
+            , alignment: .topTrailing
+        )
+    }
+}

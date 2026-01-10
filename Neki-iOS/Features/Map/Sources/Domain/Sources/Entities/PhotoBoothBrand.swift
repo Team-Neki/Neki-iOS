5e69febdfed41ebb8c16c01608e1485579548bb4
SwainYun <destap@naver.com>
2026-01-10 15:10:17 +0900

    [Chore] #29 - 디자인 시안에 준비된 브랜드만 우선 제공

--- Neki-iOS/Features/Map/Sources/Domain/Sources/Entities/PhotoBoothBrand.swift
+++ Neki-iOS/Features/Map/Sources/Domain/Sources/Entities/PhotoBoothBrand.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import DeveloperToolsSupport
 
 /// 포토부스 브랜드의 종류입니다.
 public enum PhotoBoothBrand: CaseIterable, Sendable {
@@ -13,11 +14,8 @@ public enum PhotoBoothBrand: CaseIterable, Sendable {
     case photoism
     case photogray
     case photosignature
+    case harufilm
     case planBStudio
-    case monomansion
-    case theFilm
-    case insphoto
-    case unknown
     
     public var displayName: String {
         switch self {
@@ -25,11 +23,19 @@ public enum PhotoBoothBrand: CaseIterable, Sendable {
         case .photoism: "포토이즘"
         case .photogray: "포토 그레이"
         case .photosignature: "포토 시그니처"
-        case .planBStudio: "플랜 비 스튜디오"
-        case .monomansion: "모노맨션"
-        case .theFilm: "더필름"
-        case .insphoto: "인스포토"
-        case .unknown: "비지원 브랜드"
+        case .harufilm: "하루필름"
+        case .planBStudio: "플랜비 스튜디오"
+        }
+    }
+    
+    public var logoImageResource: ImageResource {
+        switch self {
+        case .life4cut: return .imgLife4CutOriginal
+        case .photoism: return .imgPhotoismOriginal
+        case .photogray: return .imgPhotograyOriginal
+        case .photosignature: return .imgPhotosignatureOriginal
+        case .harufilm: return .imgHarufilmOriginal
+        case .planBStudio: return .imgPlanbstudioOriginal
         }
     }
 }

26ba2d569e3fb36a3c0ace53edd64041978a054f
SwainYun <destap@naver.com>
2026-01-13 04:19:57 +0900

    [Add] #35 - Auth Feature 관련 Entity, DTO 추가

---
+++ Neki-iOS/Features/Auth/Sources/Domain/Sources/Entities/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 1/12/26.
+//
+
+import Foundation
+
+/// 사용자 정보
+public struct User {
+    let nickname: String
+    let providerType: ProviderType
+}
+
+/// 로그인된 사용자의 연결 정보
+public struct UserSession {
+    let user: User
+    let tokens: AuthTokens
+}

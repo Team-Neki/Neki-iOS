fa47d1af5c46af506bfb2931a5220f870faa372c
OneTen19 <khy000426@naver.com>
2026-01-16 01:45:27 +0900

    [Chore] #39 - ChipFloatingButton 가로세로 여백 수정

--- Neki-iOS/Features/Pose/Sources/Presentation/Sources/View/PoseView.swift
+++ Neki-iOS/Features/Pose/Sources/Presentation/Sources/View/PoseView.swift
@@ -12,47 +12,71 @@ struct PoseView: View {
     
     let store: StoreOf<PoseFeature>
     
+    @State private var isFilterBarVisible: Bool = true
+    @State private var lastDragPoint: CGFloat = 0
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
+        ZStack(alignment: .top) {
             
-            filterBarView
+            ScrollView {
+                MasonryGridView(
+                    items: Array(store.items),
+                    columns: 2
+                ) { item in
+                    FeedImageView(item: item)
+                        .onTapGesture {
+                            store.send(.imageTapped(item))
+                        }
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 54)
+                .padding(.bottom, 76)
+            }
+            .scrollIndicators(.never)
+            .simultaneousGesture(
+                DragGesture()
+                    .onChanged { value in
+                        let currentPoint = value.translation.height
+                        let diff = currentPoint - lastDragPoint
+                        
+                        withAnimation(.smooth) {
+                            if diff < 0 {
+                                // 아래로 스크롤
+                                isFilterBarVisible = false
+                            } else {
+                                // 위로 스크롤
+                                isFilterBarVisible = true
+                            }
+                        }
+                        
+                        lastDragPoint = currentPoint
+                    }
+                    .onEnded {_ in
+                        lastDragPoint = 0
+                    }
+            )
+            
+            filterBar
                 .padding(.horizontal, 20)
                 .padding(.vertical, 4)
                 .padding(.bottom, 12)
+                .background(.white)                         // 뒷 배경 불투명
+//                .background(.white.opacity(0.5))          // 뒷 배경 반투명
+                .offset(y: isFilterBarVisible ? 0 : -100)   // 화면 위로 올라가며 사라지는 애니메이션
+//                .opacity(isFilterBarVisible ? 1 : 0)      // 투명화하며 사라지는 애니메이션
             
-            ZStack(alignment: .bottom) {
-                ScrollView {
-                    MasonryGridView(
-                        items: Array(store.items),
-                        columns: 2
-                    ) { item in
-                        FeedImageView(item: item)
-                            .onTapGesture {
-                                store.send(.imageTapped(item))
-                            }
-                    }
-                    .padding(.horizontal, 20)
-                    .padding(.bottom, 76)
-                }
-                .scrollIndicators(.never)
-                
+            VStack {
+                Spacer()
                 ChipFloatingButton(.randomPose) {
                     // TODO: - 랜덤포즈로 이동
                 }
-                .padding(.bottom, 24)
             }
-            
+            .padding(.bottom, 24)
+
         }
         .nekiToolbar(
-            left: .text(
-                "포즈",
-                action: nil
-            ),
-            right: .icon(
-                .iconBellFill,
-                action: {
-                    // TODO: - 알림으로 이동
-                })
+            left: .text("포즈", action: nil),
+            right: .icon(.iconBellFill, action: {})
         )
         .task {
             await store.send(.onAppear).finish()
@@ -60,11 +84,10 @@ struct PoseView: View {
     }
 }
 
-
 // MARK: - PoseView + SubViews
 
 private extension PoseView {
-    var filterBarView: some View {
+    var filterBar: some View {
         HStack(alignment: .center, spacing: 6) {
             Button("인원수") {
                 store.send(.onTapFilter)

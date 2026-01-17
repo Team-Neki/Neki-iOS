b3e21dc90c684de0b359c3d52b5babc133f69499
SwainYun <destap@naver.com>
2026-01-17 16:47:08 +0900

    [Feat] #42 - QR 스캔 기능 추가

---
+++ Neki-iOS/Features/QRCodeScanner/Sources/Presentation/Sources/Features/QRCodeScanFeature.swift
@@ -0,0 +1,67 @@
+//
+//  QRCodeScanFeature.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 1/16/26.
+//
+
+import Foundation
+import ComposableArchitecture
+import AVFoundation
+
+@Reducer
+struct QRCodeScanFeature {
+    /// QR Code 스캐너 관련 얼럿 네임스페이스
+    enum ScannerAlert {
+        case manualDownloadRequired
+    }
+    
+    @ObservableState
+    struct State {
+        var isTorchOn: Bool = false
+        var scannedURL: URL?
+        
+        var alert: ScannerAlert?
+    }
+    
+    enum Action: BindableAction {
+        // View Actions
+        case closeButtonTapped
+        case torchButtonTapped
+        case codeScanned(String)
+        
+        // Internal Actions
+        case codeDidScan(URL?)
+        
+        // Binding Actions
+        case binding(BindingAction<State>)
+    }
+    
+    @Dependency(\.dismiss) private var dismiss
+    @Dependency(\.qrScannerClient) private var scannerClient
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Reduce { (state: inout State, action: Action) -> Effect<Action> in
+            switch action {
+            case .closeButtonTapped:
+                return .run { _ in await dismiss() }
+                
+            case .torchButtonTapped:
+                state.isTorchOn.toggle()
+                return .none
+                
+            case .codeScanned(let urlString):
+                return .run { send in await send(.codeDidScan(URL(string: urlString))) }
+                
+            case .codeDidScan(let url):
+                print(url)
+                return .none
+                
+            default:
+                return .none
+            }
+        }
+    }
+}

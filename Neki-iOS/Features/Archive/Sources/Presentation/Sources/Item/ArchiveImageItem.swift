4dee678d4f65c52138f947bfc90acb67859d2ffe
OneTen19 <khy000426@naver.com>
2026-01-17 21:11:18 +0900

    [Chore] #41 - ArchiveImageItem 수정

--- Neki-iOS/Features/Archive/Sources/Presentation/Sources/Item/ArchiveImageItem.swift
+++ Neki-iOS/Features/Archive/Sources/Presentation/Sources/Item/ArchiveImageItem.swift
@@ -10,45 +10,38 @@ import Foundation
 struct ArchiveImageItem: Equatable, Identifiable {
     let id: UUID
     let imageURL: URL?
+    var isScrapped: Bool
     
-    init(id: UUID, imageURLString: String) {
+    init(id: UUID, imageURLString: String, isScrapped: Bool = false) {
         self.id = id
         self.imageURL = URL(string: imageURLString)
+        self.isScrapped = isScrapped
     }
     
-    init(id: UUID, imageURL: URL) {
+    init(id: UUID, imageURL: URL, isScrapped: Bool = false) {
         self.id = id
         self.imageURL = imageURL
+        self.isScrapped = isScrapped
     }
 }
 
 extension ArchiveImageItem {
     static func dummyData() -> [ArchiveImageItem] {
         return [
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(1)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(200)/\(500)?random=\(2)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(300)?random=\(3)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(800)?random=\(4)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(500)/\(500)?random=\(5)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(700)/\(500)?random=\(6)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(900)?random=\(7)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(100)?random=\(8)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(200)/\(500)?random=\(9)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(10)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(11)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(12)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(13)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(14)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(15)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(16)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(17)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(18)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(19)"),
-            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/\(300)/\(500)?random=\(20)")
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/300?random=1", isScrapped: true),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/500?random=2", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/300/300?random=3", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/500/300?random=4", isScrapped: true),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/500?random=5", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/300?random=6", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/700/300?random=7", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/300?random=8", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/500?random=9", isScrapped: false),
+            ArchiveImageItem(id: UUID(), imageURLString: "https://picsum.photos/200/300?random=10", isScrapped: false),
         ]
     }
     
     func toFeedImageItem() -> FeedImageItem {
-        return FeedImageItem(id: id, imageURL: imageURL!)
+        return FeedImageItem(id: id, imageURL: imageURL!, isScrapped: isScrapped)
     }
 }

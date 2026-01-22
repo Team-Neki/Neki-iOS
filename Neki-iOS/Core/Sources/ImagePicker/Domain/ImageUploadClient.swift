6c9adec58db662eb693b555fc06c1d3906cebe1d
OneTen19 <khy000426@naver.com>
2026-01-22 15:15:24 +0900

    [Feat] #53 - ImageUploadClient

--- Neki-iOS/Core/Sources/ImagePicker/Domain/ImageUploadClient.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Domain/ImageUploadClient.swift
@@ -5,4 +5,28 @@
 //  Created by OneTen on 1/21/26.
 //
 
+import ComposableArchitecture
 import Foundation
+
+public struct ImageUploadClient {
+    public var upload: @Sendable (_ data: [ImageUploadEntity], _ mediaType: ImageMediaType) async throws -> [Int]
+}
+
+extension ImageUploadClient: DependencyKey {
+    public static let liveValue: ImageUploadClient = {
+        return ImageUploadClient(
+            upload: { items, mediaType in
+                @Dependency(\.imageUploadRepository) var repository
+
+                return try await repository.upload(items: items, mediaType: mediaType)
+            }
+        )
+    }()
+}
+
+extension DependencyValues {
+    public var imageUploadClient: ImageUploadClient {
+        get { self[ImageUploadClient.self] }
+        set { self[ImageUploadClient.self] = newValue }
+    }
+}

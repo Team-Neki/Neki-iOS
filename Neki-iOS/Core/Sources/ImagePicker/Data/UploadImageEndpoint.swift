289bdb7bd51f76c0950ed460acea96a0f85a8e8a
OneTen19 <khy000426@naver.com>
2026-01-22 15:14:55 +0900

    [Feat] #53 - UploadImageEndpoint

--- Neki-iOS/Core/Sources/ImagePicker/Data/UploadImageEndpoint.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Data/UploadImageEndpoint.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public struct UploadImageEndpoint: Endpoint {
+    public var baseURL: String
+    public var path: String = ""
+    public var method: HTTPMethodType = .put
+    public var authorizationType: AuthorizationType = .none
+    public var contentType: HTTPContentType
+    
+    public var body: Encodable?
+    
+    public init(presignedUrl: String, imageData: Data, contentType: String) {
+        self.baseURL = presignedUrl
+        self.body = imageData
+        self.contentType = .custom(contentType)
+    }
+}

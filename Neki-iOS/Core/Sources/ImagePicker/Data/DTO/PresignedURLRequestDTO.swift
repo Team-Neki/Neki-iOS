7bb5570c117d7f5c5467fbb2c3c756fb3d925357
OneTen19 <khy000426@naver.com>
2026-01-22 15:14:07 +0900

    [Feat] #53 - PresignedURLRequestDTO

--- Neki-iOS/Core/Sources/ImagePicker/Data/DTO/PresignedURLRequestDTO.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Data/DTO/PresignedURLRequestDTO.swift
@@ -1,5 +1,5 @@
 //
-//  PresignedURLDTO.swift
+//  PresignedURLRequestDTO.swift
 //  Neki-iOS
 //
 //  Created by OneTen on 1/21/26.
@@ -7,23 +7,8 @@
 
 import Foundation
 
-public enum PresignedURLDTO {
-    public struct Request: Encodable {
-        public let filename: String
-        public let contentType: String
-        public let mediaType: String
-    }
-    
-    public typealias Response = BaseResponseDTO<ResponseData>
-      
-    public struct ResponseData: Decodable {
-        public let mediaID: Int
-        public let uploadURL, method, expiresIn, contentType: String
-
-        enum CodingKeys: String, CodingKey {
-            case mediaID = "mediaId"
-            case uploadURL = "uploadUrl"
-            case method, expiresIn, contentType
-        }
-    }
+public struct PresignedURLRequestDTO: Encodable {
+    public let filename: String
+    public let contentType: String
+    public let mediaType: String
 }

1ce480d8cb8b2b7edc31d43cc74c14cd07f11bb7
OneTen19 <khy000426@naver.com>
2026-01-22 15:14:45 +0900

    [Feat] #53 - PresignedEndpoint

--- Neki-iOS/Core/Sources/ImagePicker/Data/PresignedEndpoint.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Data/PresignedEndpoint.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+public struct PresignedEndpoint: Endpoint {
+    public var authorizationType: AuthorizationType = .bearer
+
+    public var contentType: HTTPContentType = .json
+    
+    public var baseURL: String {
+        guard let urlString = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            return NetworkError.invalidURLError.localizedDescription
+        }
+        return urlString
+    }
+    
+    public var path: String = "media/upload"
+    
+    public var method: HTTPMethodType = .post
+    
+    public var body: Encodable?
+    
+    public init(requests: PresignedURLRequestDTO) {
+        self.body = requests
+    }
+}

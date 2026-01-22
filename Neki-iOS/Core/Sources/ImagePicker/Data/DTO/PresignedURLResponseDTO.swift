c233ee11cd843a88838668c6736ceab9ae9a3b9f
OneTen19 <khy000426@naver.com>
2026-01-22 15:14:26 +0900

    [Feat] #53 - PresignedURLResponseDTO

--- Neki-iOS/Core/Sources/ImagePicker/Data/DTO/PresignedURLResponseDTO.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Data/DTO/PresignedURLResponseDTO.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public typealias PresignedURLResponseDTO = BaseResponseDTO<PresignedURLResponseData>
+
+public struct PresignedURLResponseData: Decodable {
+    public let mediaID: Int
+    public let uploadURL: String
+    public let method: String
+    public let expiresIn: Double
+    public let contentType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case mediaID = "mediaId"
+        case uploadURL = "uploadUrl"
+        case method, expiresIn, contentType
+    }
+}

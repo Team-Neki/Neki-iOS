4cd5de18c535c1a88ddb72c0f729d4f1169aef2e
OneTen19 <khy000426@naver.com>
2026-01-22 15:16:39 +0900

    [Feat] #53 - NekiImagePicker

--- Neki-iOS/Core/Sources/ImagePicker/Presentation/NekiImagePicker.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Presentation/NekiImagePicker.swift
@@ -5,4 +5,32 @@
 //  Created by OneTen on 1/22/26.
 //
 
-import Foundation
+import SwiftUI
+import ComposableArchitecture
+import PhotosUI
+
+public struct NekiImagePicker<Label: View>: View {
+    @Bindable var store: StoreOf<ImagePickerFeature>
+    let label: () -> Label
+    
+    public init(
+        store: StoreOf<ImagePickerFeature>,
+        @ViewBuilder label: @escaping () -> Label
+    ) {
+        self.store = store
+        self.label = label
+    }
+    
+    public var body: some View {
+        let remaining = max(0, store.maxCount - store.selectedImages.count)
+        
+        PhotosPicker(
+            selection: $store.pickerItems.sending(\.pickerItemsChanged),
+            maxSelectionCount: remaining,
+            matching: .images
+        ) {
+            label()
+        }
+        .disabled(remaining <= 0)
+    }
+}

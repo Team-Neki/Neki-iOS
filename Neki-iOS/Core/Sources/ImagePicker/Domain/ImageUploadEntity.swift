2ee23a3d16c10fa2775ba8e5e82677bcfdcb479b
OneTen19 <khy000426@naver.com>
2026-01-22 15:15:13 +0900

    [Feat] #53 - ImageUploadEntity

--- Neki-iOS/Core/Sources/ImagePicker/Domain/ImageUploadEntity.swift
+++ Neki-iOS/Core/Sources/ImagePicker/Domain/ImageUploadEntity.swift
@@ -5,4 +5,33 @@
 //  Created by OneTen on 1/22/26.
 //
 
-import Foundation
+import SwiftUI
+
+public struct ImageUploadEntity: Identifiable, @unchecked Sendable {
+    public let id: UUID
+    public let image: UIImage        // UI 표시용
+    public let data: Data            // 업로드용 Raw Data
+    public let fileExtension: String // "jpg", "png"
+    public let contentType: String      // "image/jpeg", "image/png"
+    
+    public init(
+        id: UUID = UUID(),
+        image: UIImage,
+        data: Data,
+        fileExtension: String,
+        contentType: String
+    ) {
+        self.id = id
+        self.image = image
+        self.data = data
+        self.fileExtension = fileExtension
+        self.contentType = contentType
+    }
+}
+
+public enum ImageMediaType: String, Encodable, Equatable, Sendable {
+    case userProfile = "USER_PROFILE"
+    case photoBooth = "PHOTO_BOOTH"
+    case attachment = "ATTACHMENT"
+    case temp = "TEMP"
+}

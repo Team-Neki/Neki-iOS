1cb7f5e3578b97ebe5e45fc281fffd413d546a2c
OneTen19 <khy000426@naver.com>
2026-01-26 04:19:31 +0900

    [Feat] #62 - ArchiveClient

--- Neki-iOS/Features/Archive/Sources/Domain/Sources/Client/ArchiveClient.swift
+++ Neki-iOS/Features/Archive/Sources/Domain/Sources/Client/ArchiveClient.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Dependencies
+import DependenciesMacros
+
+@DependencyClient
+struct ArchiveClient {
+    public var fetchPhotoList: (_ folderId: Int?, _ page: Int?, _ size: Int?, _ sortOrder: String?) async throws -> (photos: [PhotoEntity], hasNext: Bool)
+}
+
+extension ArchiveClient: DependencyKey {
+    static var liveValue: ArchiveClient {
+        @Dependency(\.archiveRepository) var archiveRepository
+        
+        func fetchPhotoList(_ folderId: Int?, _ page: Int?, _ size: Int?, _ sortOrder: String?) async throws -> (photos: [PhotoEntity], hasNext: Bool) {
+            let result = try await archiveRepository.fetchPhotoList(folderID: folderId, page: page, size: size, sortOrder: sortOrder)
+            
+            return result
+        }
+        
+        return ArchiveClient(fetchPhotoList: fetchPhotoList)
+    }
+}
+
+extension DependencyValues {
+    var archiveClient: ArchiveClient {
+        get { self[ArchiveClient.self] }
+        set { self[ArchiveClient.self] = newValue }
+    }
+}

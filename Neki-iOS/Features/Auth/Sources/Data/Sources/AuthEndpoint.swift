3b993496f13208e199b3d6768f89dff9c4bec5d1
SwainYun <destap@naver.com>
2026-01-26 00:31:00 +0900

    [Feat] #44 - 인증 관련 플로우 정리

--- Neki-iOS/Features/Auth/Sources/Data/Sources/AuthEndpoint.swift
+++ Neki-iOS/Features/Auth/Sources/Data/Sources/AuthEndpoint.swift
@@ -6,15 +6,54 @@
 //
 
 import Foundation
+import os
 
 enum AuthEndpoint {
     case reissueToken
-    case login(idToken: String)
+    case login(dto: SocialLoginDTO.Request, provider: ProviderType)
+    case fetchUserInfo
 }
 
 
 // MARK: - AuthEndpoint + Endpoint
-// TODO: API 명세서 확인하여 구현
-//extension AuthEndpoint: Endpoint {
-//    
-//}
+
+extension AuthEndpoint: Endpoint {
+    var authorizationType: AuthorizationType {
+        switch self {
+        case .reissueToken: return .reissue
+        case .login, .fetchUserInfo: return .bearer
+        }
+    }
+    
+    var contentType: HTTPContentType { .json }
+    
+    var baseURL: String {
+        guard let baseURLString = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            Logger.data.fault("Base URL not found in Bundle")
+            fatalError()
+        }
+        return baseURLString
+    }
+    
+    var path: String {
+        switch self {
+        case .reissueToken: return "/auth/refresh"
+        case let .login(_, provider): return "/auth/\(provider.name)/login"
+        case .fetchUserInfo: return "/users/info"
+        }
+    }
+    
+    var method: HTTPMethodType {
+        switch self {
+        case .reissueToken, .login: return .post
+        case .fetchUserInfo: return .get
+        }
+    }
+    
+    var body: (any Encodable)? {
+        switch self {
+        case .reissueToken, .fetchUserInfo: return nil
+        case let .login(dto, _): return dto
+        }
+    }
+}

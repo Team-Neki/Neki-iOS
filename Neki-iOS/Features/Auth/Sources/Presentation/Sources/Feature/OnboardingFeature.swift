1e836c212f9a3b40f8032f70b2e539dc1c273971
SwainYun <destap@naver.com>
2026-01-26 00:29:24 +0900

    [Feat] #44 - 로그인 화면 및 기능 추가 (임시)

---
+++ Neki-iOS/Features/Auth/Sources/Presentation/Sources/Feature/OnboardingFeature.swift
@@ -0,0 +1,53 @@
+//
+//  OnboardingFeature.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 1/24/26.
+//
+
+import Foundation
+import ComposableArchitecture
+import AuthenticationServices
+import os
+
+@Reducer
+public struct OnboardingFeature {
+    @ObservableState
+    public struct State {
+        
+    }
+    
+    public enum Action {
+        case kakaoLogin
+        case appleLogin(Result<ASAuthorization, any Error>)
+        case handleKakaoOpenURL(URL)
+    }
+    
+    @Dependency(\.authClient) private var authClient
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { (state: inout State, action: Action) -> Effect<Action> in
+            switch action {
+            case .kakaoLogin:
+                return .none
+                
+            case let .appleLogin(.success(authorization)):
+                guard let auth = authorization.credential as? ASAuthorizationAppleIDCredential,
+                      let idToken = auth.identityToken
+                else {
+                    Logger.presentation.error("ID Token이 비어있습니다.")
+                    return .none
+                }
+                
+                return .none
+                
+            case let .handleKakaoOpenURL(url):
+                authClient.handleKakaoOpenURL(url: url)
+                return .none
+                
+            default:
+                return .none
+            }
+        }
+    }
+}

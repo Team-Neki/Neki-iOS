50bc2ce2a3defc3f7aeab37838bfb5f863f96aec
OneTen19 <khy000426@naver.com>
2026-01-26 04:17:57 +0900

    [Feat] #52 - PhotoEntity

--- Neki-iOS/Features/Archive/Sources/Domain/Sources/Entities/PhotoEntity.swift
+++ Neki-iOS/Features/Archive/Sources/Domain/Sources/Entities/PhotoEntity.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct PhotoEntity {
+    let photoId: Int
+    let imageUrl: String
+    let folderId: Int?
+    let favorite: Bool
+    let contentType: String
+    let createdAt: String
+}

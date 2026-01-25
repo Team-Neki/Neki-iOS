8b0d831d26dd6bf4ae09843b28b366ad910430ac
OneTen19 <khy000426@naver.com>
2026-01-26 04:19:15 +0900

    [Feat] #62 - DefaultArchiveRepository

--- Neki-iOS/Features/Archive/Sources/Data/Sources/DefaultArchiveRepository.swift
+++ Neki-iOS/Features/Archive/Sources/Data/Sources/DefaultArchiveRepository.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Dependencies
+
+struct DefaultArchiveRepository: ArchiveRepository {
+    @Dependency(\.networkProvider) var networkProvider
+    
+    func fetchPhotoList(folderID: Int?, page: Int?, size: Int?, sortOrder: String?) async throws -> (photos: [PhotoEntity], hasNext: Bool) {
+        let request = PhotoListDTO.Request(folderId: folderID, page: page, size: size, sortOrder: sortOrder)
+        let endpoint = ArchiveEndpoint.getPhotoList(request: request)
+        let response: BaseResponseDTO<PhotoListDTO.PhotoListData> = try await networkProvider.request(endpoint: endpoint)
+        
+        guard let data = response.data else { throw NetworkError.responseDecodingError }
+        
+        let entities = data.toEntity()
+        
+        return (entities, data.hasNext)
+    }
+}
+
+private enum ArchiveRepositoryKey: DependencyKey {
+    static let liveValue: ArchiveRepository = DefaultArchiveRepository()
+}
+
+extension DependencyValues {
+    var archiveRepository: ArchiveRepository {
+        get { self[ArchiveRepositoryKey.self] }
+        set { self[ArchiveRepositoryKey.self] = newValue }
+    }
+}

804bc3937d6c132e758ed850983b3667dc7ee491
SwainYun <destap@naver.com>
2026-01-31 03:59:43 +0900

    [Feat] #78 - 도메인 규칙, 도메인 엔티티 정의

--- Neki-iOS/Features/Pose/Sources/Domain/Sources/Entities/PeopleCountOption.swift
+++ Neki-iOS/Features/Pose/Sources/Domain/Sources/Entities/PeopleCountOption.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // TODO: 포즈 인원수 필터 옵션 케이스 변경 예정
-public enum PeopleCountOption: Int, CaseIterable {
+public enum PeopleCountOption: Int, CaseIterable, Sendable {
     case solo = 1, duo, trio, quartet, overQuartet
     
     var displayName: String {

d9ca1a8429f9b5ed90d452945b29ab40323558d0
OneTen19 <khy000426@naver.com>
2026-02-07 00:16:08 +0900

    [Feat] #106 - Login UI

--- Neki-iOS/Core/Sources/Auth/Sources/Presentation/Sources/Coordinator/LoginCoordinatorView.swift
+++ Neki-iOS/Core/Sources/Auth/Sources/Presentation/Sources/Coordinator/LoginCoordinatorView.swift
@@ -14,10 +14,12 @@ public struct LoginCoordinatorView: View {
     public var body: some View {
         NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
             LoginView(store: store.scope(state: \.root, action: \.root))
+                .navigationBarBackButtonHidden(true)
         } destination: { store in
             switch store.case {
             case .termsAgreement(let store):
                 TermsAgreementView(store: store)
+                    .navigationBarBackButtonHidden(true)
             }
         }
     }

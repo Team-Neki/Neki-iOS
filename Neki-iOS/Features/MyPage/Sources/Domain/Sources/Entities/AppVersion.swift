b633138d41e8a5bcde6bb00e103d27dafa0ae9bd
SwainYun <destap@naver.com>
2026-02-17 19:45:49 +0900

    [Feat] #136 - 앱 버전 정보 엔티티, 유즈케이스 선언

---
+++ Neki-iOS/Features/MyPage/Sources/Domain/Sources/Entities/AppVersion.swift
@@ -0,0 +1,48 @@
+//
+//  AppVersion.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 2/17/26.
+//
+
+import Foundation
+
+struct AppVersion: Equatable, Sendable {
+    let value: String
+    
+    init(value: String) { self.value = value }
+    init(major: Int, minor: Int, revision: Int) { self.value = "\(major).\(minor).\(revision)" }
+}
+
+
+// MARK: - AppVersion + Comparable
+
+extension AppVersion: Comparable {
+    private var versionComponents: [Int] { value.split(separator: ".").compactMap { Int($0) } }
+    
+    static func < (lhs: AppVersion, rhs: AppVersion) -> Bool {
+        let lhsComponents = lhs.versionComponents
+        let rhsComponents = rhs.versionComponents
+        let maxLength = max(lhsComponents.count, rhsComponents.count)
+        
+        for i in 0..<maxLength {
+            let lhsValue = i < lhsComponents.count ? lhsComponents[i] : 0
+            let rhsValue = i < rhsComponents.count ? rhsComponents[i] : 0
+            
+            guard lhsValue != rhsValue else { continue }
+            return lhsValue < rhsValue
+        }
+        
+        return false
+    }
+}
+
+/// 업데이트가 요구되는 상태를 의미합니다.
+enum AppUpdateStatus: Equatable, Sendable {
+    /// 업데이트 필요
+    case mustUpdate
+    /// 업데이트 권장
+    case optionalUpdate
+    /// 업데이트 불필요(최신)
+    case upToDate
+}

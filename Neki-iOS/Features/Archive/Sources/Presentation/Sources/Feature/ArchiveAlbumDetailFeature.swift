570d87151f4e2529322cee07639c67d818ee4865
OneTen19 <khy000426@naver.com>
2026-04-12 23:18:41 +0900

    [Chore] #202 -  imageURL들을 꺼내오는 작업을 백그라운드 스레드 과정에 포함시켜 메인스레드의 부담 완화

--- Neki-iOS/Features/Archive/Sources/Presentation/Sources/Feature/ArchiveAlbumDetailFeature.swift
+++ Neki-iOS/Features/Archive/Sources/Presentation/Sources/Feature/ArchiveAlbumDetailFeature.swift
@@ -292,8 +292,9 @@ struct ArchiveAlbumDetailFeature {
             case .onTapDownloadButton:
                 guard !state.selectedIDs.isEmpty else { return .none }
                 state.isLoading = true
-                let urls = state.selectedIDs.compactMap { state.photos[id: $0]?.imageURL }
-                return .run { send in
+                return .run { [photos = state.photos, selectedIDs = state.selectedIDs] send in
+                    let urls = selectedIDs.compactMap { photos[id: $0]?.imageURL }
+                    
                     let count = try await imageDownloadClient.downloadImages(urls: urls)
                     await send(.downloadImagesResponse(successCount: count))
                 }

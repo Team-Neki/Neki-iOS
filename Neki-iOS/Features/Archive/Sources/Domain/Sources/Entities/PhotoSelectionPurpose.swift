199e20afc91f98d4544d2af40e8f2a92c860cd9c
OneTen19 <khy000426@naver.com>
2026-04-13 23:53:25 +0900

    [Chore] #202 - PhotoSelectionPurpose 레이어 변경

---
+++ Neki-iOS/Features/Archive/Sources/Domain/Sources/Entities/PhotoSelectionPurpose.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoSelectionPurpose.swift
+//  Neki-iOS
+//
+//  Created by OneTen on 4/13/26.
+//
+
+import Foundation
+
+public enum PhotoSelectionPurpose {
+    case duplicate
+    case move
+}

fa8c790daec3986d412c2b296c7f3fcfd96d2a27
SwainYun <destap@naver.com>
2026-04-19 18:23:21 +0900

    [Chore] #214 - NekiToolbar 선언부 수정

--- Neki-iOS/Features/Archive/Sources/Presentation/Sources/View/ArchivePhotoDetailView.swift
+++ Neki-iOS/Features/Archive/Sources/Presentation/Sources/View/ArchivePhotoDetailView.swift
@@ -21,13 +21,6 @@ struct ArchivePhotoDetailView: View {
             photoTabView
                 .ignoresSafeArea()
             
-            // 툴바
-            VStack(spacing: 0) {
-                topToolbar
-                    .background(.white)
-                Spacer()
-            }
-            
             // 딤처리
             dimmedBackground
                 .ignoresSafeArea()
@@ -61,6 +54,20 @@ struct ArchivePhotoDetailView: View {
                 .zIndex(10)
             }
         }
+        .nekiToolbar(
+            backgroundColor: Color(.systemBackground),
+            left: { NekiToolBar.back { store.send(.onTapBackButton) } },
+            center: { NekiToolBar.textCenter(store.formattedDate) },
+            right: {
+                Button {
+                    store.send(.toggleDropDownMenu)
+                } label: {
+                    Image(.iconEllipsis)
+                        .frame(width: 24, height: 24)
+                        .padding(8)
+                }
+            }
+        )
         .onChange(of: store.currentItemID) { _, _ in
             store.send(.closeDropDownMenu)
             store.send(.binding(.set(\.isMemoVisible, false)))
@@ -113,24 +120,6 @@ struct ArchivePhotoDetailView: View {
 // MARK: - Subviews
 
 extension ArchivePhotoDetailView {
-    // 툴바
-    private var topToolbar: some View {
-        NekiToolbarLayout(
-            backgroundColor: Color(.systemBackground),
-            left: { NekiToolBar.back { store.send(.onTapBackButton) } },
-            center: { NekiToolBar.textCenter(store.formattedDate) },
-            right: {
-                Button {
-                    store.send(.toggleDropDownMenu)
-                } label: {
-                    Image(.iconEllipsis)
-                        .frame(width: 24, height: 24)
-                        .padding(8)
-                }
-            }
-        )
-    }
-    
     private var dropDownMenu: some View {
         VStack(alignment: .leading, spacing: 4) {
             Button {

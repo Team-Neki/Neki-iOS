23a7b271ef9e4c4ceeab4d1ffb14388e61d12a5c
SwainYun <destap@naver.com>
2026-04-19 18:22:53 +0900

    [Refactor] #214 - ToolbarContent 프로토콜 기반 리팩토링, 스와이프로 뒤로가기 제스처 수신

--- Neki-iOS/Shared/DesignSystem/Sources/Component/ToolBar/NekiToolBar.swift
+++ Neki-iOS/Shared/DesignSystem/Sources/Component/ToolBar/NekiToolBar.swift
@@ -114,38 +114,111 @@ extension NekiToolBar {
     }
 }
 
-public struct NekiToolbarLayout<Left: View, Center: View, Right: View>: View {
+public struct NekiToolbarContent<Left: View, Center: View, Right: View>: ToolbarContent {
     let left: Left
     let center: Center
     let right: Right
-    let backgroundColor: Color
     
     public init(
-        backgroundColor: Color = .white,
         @ViewBuilder left: () -> Left,
         @ViewBuilder center: () -> Center,
         @ViewBuilder right: () -> Right
     ) {
-        self.backgroundColor = backgroundColor
         self.left = left()
         self.center = center()
         self.right = right()
     }
     
-    public var body: some View {
-        ZStack(alignment: .center) {
-            HStack(alignment: .center, spacing: 0) {
-                left
-                Spacer()
-                right
-            }
-            .padding(.leading, 8)
-            .padding(.trailing, 20)
-            .frame(height: 54)
-            
+    public var body: some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            left
+                .offset(x: -8)
+        }
+        ToolbarItem(placement: .principal) {
             center
         }
-        .background(backgroundColor)
-        .frame(height: 54)
+        ToolbarItem(placement: .topBarTrailing) {
+            right
+                .offset(x: -4)
+        }
+    }
+}
+
+public extension View {
+    func nekiToolbar<Left: View, Center: View, Right: View>(
+        backgroundColor: Color? = nil,
+        isOverlay: Bool = false,
+        @ViewBuilder left: () -> Left = { EmptyView() },
+        @ViewBuilder center: () -> Center = { EmptyView() },
+        @ViewBuilder right: () -> Right = { EmptyView() }
+    ) -> some View {
+        self
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar { NekiToolbarContent(left: left, center: center, right: right) }
+            .background(NekiNavigationConfigurator(backgroundColor: backgroundColor, isOverlay: isOverlay))
+    }
+}
+
+struct NekiNavigationConfigurator: UIViewControllerRepresentable {
+    var backgroundColor: Color?
+    var isOverlay: Bool
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        ConfiguratorViewController(backgroundColor: backgroundColor, isOverlay: isOverlay)
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        guard let viewController = uiViewController as? ConfiguratorViewController else { return }
+        viewController.backgroundColor = backgroundColor
+        viewController.isOverlay = isOverlay
+        viewController.updateAppearance()
+    }
+
+    final class ConfiguratorViewController: UIViewController, UIGestureRecognizerDelegate {
+        var backgroundColor: Color?
+        var isOverlay: Bool
+
+        init(backgroundColor: Color?, isOverlay: Bool) {
+            self.backgroundColor = backgroundColor
+            self.isOverlay = isOverlay
+            super.init(nibName: nil, bundle: nil)
+        }
+
+        required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .clear
+        }
+
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            navigationController?.interactivePopGestureRecognizer?.delegate = self
+            updateAppearance()
+        }
+
+        func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+            (navigationController?.viewControllers.count ?? 0) > 1
+        }
+
+        func updateAppearance() {
+            guard let navigationController = navigationController else { return }
+            let appearance = UINavigationBarAppearance()
+            
+            if isOverlay {
+                appearance.configureWithTransparentBackground()
+            } else {
+                appearance.configureWithOpaqueBackground()
+                appearance.backgroundColor = UIColor(backgroundColor ?? .white)
+            }
+            
+            appearance.shadowColor = .clear
+            appearance.shadowImage = nil
+
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+            navigationController.navigationBar.compactAppearance = appearance
+        }
     }
 }

004e77a455130d4f2ecf104ae45c08e151b0cf5f
SwainYun <destap@naver.com>
2026-07-09 00:26:43 +0900

    [Refactor] PushNotification 구현 의존성 분리

--- Neki-iOS/Core/Sources/Notification/Sources/Domain/Sources/Interfaces/PushNotificationRepository.swift
+++ Neki-iOS/Core/Sources/Notification/Sources/Domain/Sources/Interfaces/PushNotificationRepository.swift
@@ -13,8 +13,14 @@ protocol PushNotificationRepository {
     func requestAuthorization() async throws -> Bool
     func fetchNotificationList() async throws -> [PushNotificationListItem]
     func fetchFCMToken() async throws -> PushNotificationToken
+    func checkAPNSTokenRegistration() -> Bool
+    func fetchCurrentAPNSToken() -> String?
+    func fetchCurrentFCMToken() -> String?
+    func configureMessaging()
     func updateAPNSToken(_ token: Data)
     func processReceivedNotification(_ payload: PushNotificationPayload)
+    func events() async -> AsyncStream<PushNotificationEvent>
+    func publishEvent(_ event: PushNotificationEvent) async
     func updateDeviceToken(
         _ token: PushNotificationToken,
         isPushNotificationAgreed: Bool

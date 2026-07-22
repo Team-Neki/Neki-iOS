9f6de31dd910a64e09e7a3b4f39df9bad4c6e965
SwainYun <destap@naver.com>
2026-07-22 14:53:28 +0900

    [Refactor] Share Extension 환경 설정 주입

--- NekiShareExtension/Sources/Domain/Sources/Entities/ShareExtensionConfiguration.swift
+++ NekiShareExtension/Sources/Domain/Sources/Entities/ShareExtensionConfiguration.swift
@@ -12,17 +12,26 @@ enum ShareExtensionConfigurationError: Error {
 }
 
 struct ShareExtensionConfiguration {
+    private enum InfoKey {
+        static let appGroupID = "NEKI_APP_GROUP_ID"
+        static let urlScheme = "NEKI_URL_SCHEME"
+    }
+
     let appGroupID: String
     let deepLinkURL: URL?
     
     init(bundle: Bundle = .main) {
-        #if DEBUG
-        self.appGroupID = "group.com.Neki-dev.Share-Extension"
-        let scheme = "neki-dev"
-        #else
-        self.appGroupID = "group.com.OneTen.Neki-iOS.Share-Extension"
-        let scheme = "neki"
-        #endif
+        guard let appGroupID = bundle.object(forInfoDictionaryKey: InfoKey.appGroupID) as? String,
+              let scheme = bundle.object(forInfoDictionaryKey: InfoKey.urlScheme) as? String,
+              appGroupID.isEmpty == false,
+              scheme.isEmpty == false
+        else {
+            self.appGroupID = ""
+            self.deepLinkURL = nil
+            return
+        }
+
+        self.appGroupID = appGroupID
         
         var components = URLComponents(string: "\(scheme)://shareExtension")
         components?.queryItems = [URLQueryItem(name: "appGroupID", value: appGroupID)]

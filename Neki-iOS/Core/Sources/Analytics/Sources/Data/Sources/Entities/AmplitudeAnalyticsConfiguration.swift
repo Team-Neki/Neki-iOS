aa7fbb37395c8ef946fb12c71329db23431e8ea8
SwainYun <destap@naver.com>
2026-08-19 00:47:18 +0900

    [Docs] #336 - 히스토리 추적을 위한 주석 추가 [리뷰 반영]

--- Neki-iOS/Core/Sources/Analytics/Sources/Data/Sources/Entities/AmplitudeAnalyticsConfiguration.swift
+++ Neki-iOS/Core/Sources/Analytics/Sources/Data/Sources/Entities/AmplitudeAnalyticsConfiguration.swift
@@ -9,6 +9,8 @@ import Foundation
 
 struct AmplitudeAnalyticsConfiguration: Sendable {
     private enum Constants {
+        /// Amplitude 이벤트 추적에 필요한 ID의 최소길이
+        /// - Note: 최신 Amplitude의 API는 다섯 자 이상이어야 정상 수신되지만, Neki의 사용자ID는 양의 정수이므로 최대 식별자 길이를 1로 설정하는 것이 필수입니다.
         static let minimumIdentifierLength = 1
     }
 

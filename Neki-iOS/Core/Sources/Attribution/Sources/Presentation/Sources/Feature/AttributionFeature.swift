5c70508ebd70f83f5591832c48e0b43f1f10417d
SwainYun <destap@naver.com>
2026-08-20 19:36:53 +0900

    [Feat] #343 ATT 권한 요청 상태 관리

--- Neki-iOS/Core/Sources/Attribution/Sources/Presentation/Sources/Feature/AttributionFeature.swift
+++ Neki-iOS/Core/Sources/Attribution/Sources/Presentation/Sources/Feature/AttributionFeature.swift
@@ -5,25 +5,58 @@
 //  Created by SwainYun on 7/30/26.
 //
 
+import AppTrackingTransparency
 import ComposableArchitecture
 
 @Reducer
 struct AttributionFeature {
     @ObservableState
-    struct State: Equatable {}
+    struct State: Equatable {
+        var isRequestingTrackingAuthorization = false
+    }
     
     enum Action {
         case appLaunched
+        case requestTrackingAuthorization
+        case trackingAuthorizationStatusResponse(ATTrackingManager.AuthorizationStatus)
+        case trackingAuthorizationRequestCompleted
         case completeRegistration
     }
     
     @Dependency(\.attributionClient) private var attributionClient
     
     var body: some ReducerOf<Self> {
-        Reduce { _, action in
+        Reduce { state, action in
             switch action {
             case .appLaunched:
                 return .run { _ in await attributionClient.initializeAttribution() }
+
+            case .requestTrackingAuthorization:
+                guard state.isRequestingTrackingAuthorization == false else { return .none }
+                state.isRequestingTrackingAuthorization = true
+                return .run { send in
+                    let status = await attributionClient.checkTrackingAuthorizationStatus()
+                    await send(.trackingAuthorizationStatusResponse(status))
+                }
+
+            case let .trackingAuthorizationStatusResponse(status):
+                switch status {
+                case .notDetermined:
+                    return .run { send in
+                        await attributionClient.requestTrackingAuthorization()
+                        await send(.trackingAuthorizationRequestCompleted)
+                    }
+                case .restricted, .denied, .authorized:
+                    state.isRequestingTrackingAuthorization = false
+                    return .none
+                @unknown default:
+                    state.isRequestingTrackingAuthorization = false
+                    return .none
+                }
+
+            case .trackingAuthorizationRequestCompleted:
+                state.isRequestingTrackingAuthorization = false
+                return .none
 
             case .completeRegistration:
                 return .run { _ in await attributionClient.trackCompleteRegistration() }

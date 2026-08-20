b95f45a3203891b2a601bab35919f88a8cd50eb5
SwainYun <destap@naver.com>
2026-08-20 19:36:44 +0900

    [Feat] #343 ATT 권한 인터페이스 추가

--- Neki-iOS/Core/Sources/Attribution/Sources/Domain/Sources/Clients/AttributionClient.swift
+++ Neki-iOS/Core/Sources/Attribution/Sources/Domain/Sources/Clients/AttributionClient.swift
@@ -5,10 +5,15 @@
 //  Created by SwainYun on 7/30/26.
 //
 
+import AppTrackingTransparency
 import DependenciesMacros
 
 @DependencyClient
 public struct AttributionClient {
+    public typealias AuthorizationStatus = ATTrackingManager.AuthorizationStatus
+
     public var initializeAttribution: @Sendable () async -> Void = {}
+    public var checkTrackingAuthorizationStatus: @MainActor @Sendable () -> AuthorizationStatus = { .notDetermined }
+    public var requestTrackingAuthorization: @MainActor @Sendable () async -> Void = {}
     public var trackCompleteRegistration: @Sendable () async -> Void = {}
 }
--- Neki-iOS/Core/Sources/Attribution/Sources/Domain/Sources/Interfaces/AttributionRepository.swift
+++ Neki-iOS/Core/Sources/Attribution/Sources/Domain/Sources/Interfaces/AttributionRepository.swift
@@ -5,7 +5,11 @@
 //  Created by SwainYun on 7/30/26.
 //
 
+import AppTrackingTransparency
+
 public protocol AttributionRepository: Sendable {
     func initializeAttribution() async
+    @MainActor func checkTrackingAuthorizationStatus() -> ATTrackingManager.AuthorizationStatus
+    @MainActor func requestTrackingAuthorization() async
     func trackCompleteRegistration() async
 }

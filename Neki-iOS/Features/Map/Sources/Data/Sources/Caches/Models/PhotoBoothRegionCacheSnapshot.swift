4ee4f1e3e7e3295533e004a75b6ff90104327d5c
SwainYun <destap@naver.com>
2026-08-30 16:41:17 +0900

    [Refactor] #356 POI 조회를 지역 단일 요청으로 전환

---
+++ Neki-iOS/Features/Map/Sources/Data/Sources/Caches/Models/PhotoBoothRegionCacheSnapshot.swift
@@ -0,0 +1,11 @@
+//
+//  PhotoBoothRegionCacheSnapshot.swift
+//  Neki-iOS
+//
+//  Created by SwainYun on 8/30/26.
+//
+
+struct PhotoBoothRegionCacheSnapshot: Sendable {
+    let photoBooths: [PhotoBooth]
+    let metadata: PhotoBoothCacheMetadata
+}

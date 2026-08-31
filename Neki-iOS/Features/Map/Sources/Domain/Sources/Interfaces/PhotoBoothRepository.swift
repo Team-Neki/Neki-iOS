2b178768d35862a7ea1743b0a59845506753ab65
SwainYun <destap@naver.com>
2026-09-01 04:43:08 +0900

    [Docs] #356 POI 조회 스트림 오류 계약 정리 [리뷰 반영]

--- Neki-iOS/Features/Map/Sources/Domain/Sources/Interfaces/PhotoBoothRepository.swift
+++ Neki-iOS/Features/Map/Sources/Domain/Sources/Interfaces/PhotoBoothRepository.swift
@@ -11,8 +11,7 @@ import Dependencies
 protocol PhotoBoothRepository {
     /// 특정 지도 영역 내의 포토부스 목록을 가져옵니다.
     /// - Parameter bounds: 조회 시점의 지리적 영역
-    /// - Returns: 해당 영역 내의 포토부스 배열 스트림
-    /// - Throws: 캐시와 네트워크 모두에서 유효한 타일을 확보하지 못한 경우의 조회 오류
+    /// - Returns: 조회 영역을 포함하는 포토부스 배열 스트림. 조회 실패는 스트림을 순회할 때 전달됩니다.
     func readPhotoBooths(in bounds: GeographicBoundingBox) async -> AsyncThrowingStream<[PhotoBooth], Error>
 
     /// 특정 포토부스의 즐겨찾기 상태를 변경합니다.

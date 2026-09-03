a25e20398b7e6dfb57bf5ed54c05f5d32714a47e
J.H. Moon <moonjh587@naver.com>
2026-09-04 02:47:29 +0900

    [Refactor] #352 검색 도메인 계약을 확정 API 규격에 맞춰 재정의

--- Neki-iOS/Features/Map/Sources/Domain/Sources/Entities/PhotoBoothSearchQuery.swift
+++ Neki-iOS/Features/Map/Sources/Domain/Sources/Entities/PhotoBoothSearchQuery.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-/// 사용자가 제출한 검색어 원문입니다.
+/// 사용자가 제출한 검색어입니다.
 ///
-/// 클라이언트에서 정규화하거나 유효성을 판정하지 않고 입력값을 그대로 보존합니다.
+/// 서버 검색은 접두 일치이므로 입력한 값을 다듬지 않고 그대로 담습니다.
+/// 공백뿐인 검색어를 거절할지는 서버가 판정하므로 클라이언트에서 미리 걸러내지 않습니다.
 public struct PhotoBoothSearchQuery: RawRepresentable, Equatable, Sendable {
     public let rawValue: String
 
